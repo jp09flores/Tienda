c85f5828ec47e963de247207c31752d82536011d
jp09f <jp09f@LAPTOP-L9EACJ37>
2023-07-21 01:33:58 -0600

    commit semana 10

---
+++ src/main/java/com/Tienda/domain/Rol.java
@@ -0,0 +1,29 @@
+package com.Tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="rol")
+public class Rol implements Serializable{
+    
+    private static final long serialVersionUID =1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_rol")
+    private Long idRol;
+    
+    @NotEmpty
+    private String nombre;
+    
+    @Column(name="id_usuario")
+    private Long idUsuario;
+    
+    
+    
+    
+}
